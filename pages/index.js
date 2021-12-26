b7ef34db40398211736b14fce25c849c201f1cb4
Praveen Prajapati <praveenprajapati0810@gmail.com>
2021-12-26 16:12:36 +0530

    add 3d model

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,37 @@
-import Head, { defaultHead } from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
-// import styles from '../styles/Home.module.css';
-// import styles from '../styles/nav.module.css';
 import Navbar from './components/Navbar';
+import React, { Suspense, useRef, useEffect, useState } from 'react';
+import { Canvas } from 'react-three-fiber';
+import { OrbitControls, useGLTF, useAnimations } from '@react-three/drei';
+
+export function Model(props) {
+  // const { scene } = useGLTF('../scene.gltf');
+  // return <primitive object={scene} />;
+
+  const group = useRef();
+  const mesh = useRef();
+  const { nodes, materials, animations } = useGLTF('/scene.gltf', '/scene.bin');
+  const { actions } = useAnimations(animations, group);
+
+  const { scene } = useGLTF('/scene.gltf');
+  return <primitive object={scene} />;
+}
+
+useGLTF.preload('/scene.gltf');
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Home Page</title>
-      </Head>
       <Navbar />
       <h1>Welcome Home Baby</h1>
+
+      <Canvas pixelRatio={[1, 2]} camera={{ position: [-10, 15, 15], fov: 50 }}>
+        <ambientLight intensity={0.6} />
+        <directionalLight intensity={0.5} />
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
     </>
   );
 }
